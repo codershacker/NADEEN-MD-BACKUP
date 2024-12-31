8833d004c633273cb9925406ea66a9847dee4d99
Nadeenpoorna-max <nadeenpoorna5@gmail.com>
2024-12-31 23:47:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "", //put your session id
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/2nmi9q.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello I am nadeen poorna*",
-MONGODB: process.env.MONGODB || "mongodb+srv://lithumhelitha2008:7rDwXUVeTnIfmnjl@cluster0.1d0bq.mongodb.net/",
+MONGODB: process.env.MONGODB || "mongodb://mongo:oQGLPajGinTwVcMqubCaSuAJpKAtURiJ@junction.proxy.rlwy.net:31477",
 };
